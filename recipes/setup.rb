44a6dba3857db2bdb9f457efa13e06661aa5581e
vagrant <vagrant@layer201.prod.mint.com>
2019-01-15 09:12:06 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'tree' do
+  action:install
+end
+
+package 'git' do 
+  action:install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+  content 'This is Gaurab Dey learning Chef'
+  action:create
+end
+
+service 'ntpd' do
+  action [:enable, :start]
+end
